6738d7b5d89985e9908b6d8ffaf302bd9c0e73a3
Aminah Statham-Adams <97044463+aminahSA@users.noreply.github.com>
2024-02-16 14:26:46 -0500

    more stuff

---
+++ app/src/main/java/Product.kt
@@ -0,0 +1,8 @@
+
+data class Product(
+    val name: String,
+    val category: String,
+    val expiryDate: String?,
+    val price: Int)
+
+//listOf("Treadmill", "Equipment", null, 32),
--- app/src/main/java/com/cs4520/assignment1/ProductAdapter.kt
+++ app/src/main/java/com/cs4520/assignment1/ProductAdapter.kt
@@ -1,19 +1,28 @@
 package com.cs4520.assignment1
 
+import Product
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class ProductAdapter(private val products: List<String>) : RecyclerView.Adapter<ProductViewHolder>() {
+class ProductAdapter(
+    private val products: List<Product>)
+    : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
+
+
+        class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.product, parent, false)
-        return ProductViewHolder(view)
+        return ProductViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.product, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
         val product = products[position]
-        holder.bind(product)
     }
 
     override fun getItemCount(): Int {
